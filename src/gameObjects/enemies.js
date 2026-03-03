c59de8271910473abc6ade5e26c0d2a611a11acf
mouadh <wannassii.mouadh@gmail.com>
2026-03-03 17:09:49 +0100

    level 1

--- src/gameObjects/enemies.js
+++ src/gameObjects/enemies.js
@@ -55,7 +55,7 @@ export class EnemyManager {
     newEnemy.maxX = original.maxX;
 
     // 🔥 Small visual offset ONLY on position (not range)
-    newEnemy.x += 40;
+    newEnemy.x += 150;
 
     return newEnemy;
   }
